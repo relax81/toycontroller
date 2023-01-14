6a5b5e2f3c78a6388b3f00f4fbb38b68a0639f4d
Leif <cerberus81@gmail.com>
2023-01-14 15:46:46 +0100

    working start

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -9,11 +9,20 @@
 #endif
 
 #include <Arduino.h>
+#include "true-credentials.h"
 #include <U8g2lib.h>
 #include <Wire.h>
 #include <TickTwo.h>
 #include <AiEsp32RotaryEncoder.h>
 #include <pinout.h>
+#include <DNSServer.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
+#include <Arduino_JSON.h>
+
+
 
 bool buttonPressed = false;
 int encoderPosition = 0;
@@ -51,6 +60,104 @@ bool pwm2_enabled = false;
 bool pwm3_enabled = false;
 bool pwm4_enabled = false;
 bool buzzer_enabled = true;
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
+String message = "";
+String sliderValue1 = "0";
+String sliderValue2 = "0";
+String sliderValue3 = "0";
+//Json Variable to Hold Slider Values
+JSONVar sliderValues;
+//Get Slider Values
+String getSliderValues(){
+  sliderValues["sliderValue1"] = String(sliderValue1);
+  sliderValues["sliderValue2"] = String(sliderValue2);
+  sliderValues["sliderValue3"] = String(sliderValue3);
+
+  String jsonString = JSON.stringify(sliderValues);
+  return jsonString;
+}
+// Initialize SPIFFS
+void initFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  else{
+   Serial.println("SPIFFS mounted successfully");
+  }
+}
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+void notifyClients(String sliderValues) {
+  ws.textAll(sliderValues);
+}
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    message = (char*)data;
+    if (message.indexOf("1s") >= 0) {
+      sliderValue1 = message.substring(2);
+      Ch130vOn =sliderValue1.toInt();
+      Serial.println(Ch130vOn);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }
+    if (message.indexOf("2s") >= 0) {
+      sliderValue2 = message.substring(2);
+      Ch130vOff = sliderValue2.toInt();
+      Serial.println(Ch130vOff);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }    
+    if (message.indexOf("3s") >= 0) {
+      sliderValue3 = message.substring(2);
+      Ch130vPWM = sliderValue3.toInt();
+      Serial.println(Ch130vPWM);
+      Serial.print(getSliderValues());
+      notifyClients(getSliderValues());
+    }
+    if (strcmp((char*)data, "getValues") == 0) {
+      notifyClients(getSliderValues());
+    }
+  }
+}
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+
+
 
 
 //Encoder
@@ -401,6 +508,7 @@ void setup() {
   Serial.begin(115200);
   debugln("setup started");
 
+
   // Pins
   pinMode(buzzerPin, OUTPUT);
   pinMode(wsLED, OUTPUT);
@@ -453,6 +561,19 @@ void setup() {
   displayMenu();
   displayValues();
   u8g2.sendBuffer();
+
+  initFS();
+  initWiFi();
+  initWebSocket();
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  
+  server.serveStatic("/", SPIFFS, "/");
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
@@ -527,4 +648,5 @@ void loop() {
 
   // Buzzer test
   // ledcWriteTone(buzzer,4000);
+  ws.cleanupClients();
 }
